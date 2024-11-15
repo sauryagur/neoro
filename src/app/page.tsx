f04cb611cbeb85171255eb4be119a99e7d802681
Saurya Gur <saurya.gur@outlook.com>
2024-11-16 04:00:56 +0530

    Added Menubar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,123 @@
 import Ripple from "@/components/ui/ripple";
-import { Button } from "@/components/ui/button"
-import { Textarea } from "@/components/ui/textarea"
+import {Button} from "@/components/ui/button"
+import {Textarea} from "@/components/ui/textarea"
+import {
+    Menubar,
+    MenubarCheckboxItem,
+    MenubarContent,
+    MenubarItem,
+    MenubarMenu,
+    MenubarRadioGroup,
+    MenubarRadioItem,
+    MenubarSeparator,
+    MenubarShortcut,
+    MenubarSub,
+    MenubarSubContent,
+    MenubarSubTrigger,
+    MenubarTrigger,
+} from "@/components/ui/menubar"
+
+
 
 export default function Home() {
     return (
         <>
+            <Menubar>
+                <MenubarMenu>
+                    <MenubarTrigger>File</MenubarTrigger>
+                    <MenubarContent>
+                        <MenubarItem>
+                            New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+                        </MenubarItem>
+                        <MenubarItem>
+                            New Window <MenubarShortcut>⌘N</MenubarShortcut>
+                        </MenubarItem>
+                        <MenubarItem disabled>New Incognito Window</MenubarItem>
+                        <MenubarSeparator />
+                        <MenubarSub>
+                            <MenubarSubTrigger>Share</MenubarSubTrigger>
+                            <MenubarSubContent>
+                                <MenubarItem>Email link</MenubarItem>
+                                <MenubarItem>Messages</MenubarItem>
+                                <MenubarItem>Notes</MenubarItem>
+                            </MenubarSubContent>
+                        </MenubarSub>
+                        <MenubarSeparator />
+                        <MenubarItem>
+                            Print... <MenubarShortcut>⌘P</MenubarShortcut>
+                        </MenubarItem>
+                    </MenubarContent>
+                </MenubarMenu>
+                <MenubarMenu>
+                    <MenubarTrigger>Edit</MenubarTrigger>
+                    <MenubarContent>
+                        <MenubarItem>
+                            Undo <MenubarShortcut>⌘Z</MenubarShortcut>
+                        </MenubarItem>
+                        <MenubarItem>
+                            Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
+                        </MenubarItem>
+                        <MenubarSeparator />
+                        <MenubarSub>
+                            <MenubarSubTrigger>Find</MenubarSubTrigger>
+                            <MenubarSubContent>
+                                <MenubarItem>Search the web</MenubarItem>
+                                <MenubarSeparator />
+                                <MenubarItem>Find...</MenubarItem>
+                                <MenubarItem>Find Next</MenubarItem>
+                                <MenubarItem>Find Previous</MenubarItem>
+                            </MenubarSubContent>
+                        </MenubarSub>
+                        <MenubarSeparator />
+                        <MenubarItem>Cut</MenubarItem>
+                        <MenubarItem>Copy</MenubarItem>
+                        <MenubarItem>Paste</MenubarItem>
+                    </MenubarContent>
+                </MenubarMenu>
+                <MenubarMenu>
+                    <MenubarTrigger>View</MenubarTrigger>
+                    <MenubarContent>
+                        <MenubarCheckboxItem>Always Show Bookmarks Bar</MenubarCheckboxItem>
+                        <MenubarCheckboxItem checked>
+                            Always Show Full URLs
+                        </MenubarCheckboxItem>
+                        <MenubarSeparator />
+                        <MenubarItem inset>
+                            Reload <MenubarShortcut>⌘R</MenubarShortcut>
+                        </MenubarItem>
+                        <MenubarItem disabled inset>
+                            Force Reload <MenubarShortcut>⇧⌘R</MenubarShortcut>
+                        </MenubarItem>
+                        <MenubarSeparator />
+                        <MenubarItem inset>Toggle Fullscreen</MenubarItem>
+                        <MenubarSeparator />
+                        <MenubarItem inset>Hide Sidebar</MenubarItem>
+                    </MenubarContent>
+                </MenubarMenu>
+                <MenubarMenu>
+                    <MenubarTrigger>Social Links</MenubarTrigger>
+                    <MenubarContent>
+                        <MenubarRadioGroup value="benoit">
+                            <MenubarRadioItem value="andy">Andy</MenubarRadioItem>
+                            <MenubarRadioItem value="benoit">Benoit</MenubarRadioItem>
+                            <MenubarRadioItem value="Luis">Luis</MenubarRadioItem>
+                        </MenubarRadioGroup>
+                        <MenubarSeparator />
+                        <MenubarItem inset>Edit...</MenubarItem>
+                        <MenubarSeparator />
+                        <MenubarItem inset>Add Profile...</MenubarItem>
+                    </MenubarContent>
+                </MenubarMenu>
+            </Menubar>
+
             <div
-                className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
+                className="relative flex h-[450px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
                 <Ripple/>
 
             </div>
             <div className="grid w-full gap-2">
-                <Textarea placeholder="Type your message here."/>
-                <Button>Send message</Button>
+                <Textarea placeholder="Type your message here." className={"border-4"}/>
+                <Button type="submit">Send</Button>
             </div>
         </>
     );
