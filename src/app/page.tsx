182fff2f82ee6107bf5a5c996807b673351f31c2
Saurya Gur <saurya.gur@outlook.com>
2024-11-16 02:32:43 +0530

    Theme dark and added textbar.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,9 @@ import Ripple from "@/components/ui/ripple";
 
 
 export default function Home() {
-  return (
-
-
-      <div
-          className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
-          <p className="z-10 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-white">
-          </p>
-          <Ripple/>
-      </div>
-
-
-  );
+    return (
+        <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
+            <Ripple />
+        </div>
+    );
 }
---
+++ src/components/ui/textbar.tsx
@@ -0,0 +1,7 @@
+export default function Textbar(){
+    return (
+        <>
+            <input type="text"/>
+        </>
+    )
+}
